d9117d633becf807ad18666134f6b9d89b9b3969
Alexander Riccio <alexander@riccio.com>
2015-06-05 17:50:37 -0400

    fixed sqlite3_vsnprintf return value annotation
    sqlite3StrAccumFinish CAN return NULL, thus sqlite3_vsnprintf CAN ALSO
    return NULL

--- sqlite-amalgamation-3080900/sqlite3.h
+++ sqlite-amalgamation-3080900/sqlite3.h
@@ -2363,7 +2363,7 @@ SQLITE_API void SQLITE_STDCALL sqlite3_free_table(_Post_ptr_invalid_ char **resu
 SQLITE_API _Ret_maybenull_z_ const char* SQLITE_CDECL sqlite3_mprintf(_Printf_format_string_ _In_z_ const char*,...);
 SQLITE_API _Ret_maybenull_z_ const char* SQLITE_STDCALL sqlite3_vmprintf(_Printf_format_string_ _In_z_ const char*, va_list);
 SQLITE_API _Ret_z_ const char* SQLITE_CDECL sqlite3_snprintf(_In_range_( >=, 1 ) int n, _Pre_writable_size_( n ) char*,_Printf_format_string_ _In_z_ const char*, ...);
-SQLITE_API _Ret_z_ const char* SQLITE_STDCALL sqlite3_vsnprintf(_In_range_( >=, 1 ) int n,_Pre_writable_size_( n ) char*, _Printf_format_string_ _In_z_ const char*, va_list);
+SQLITE_API _Ret_maybenull_z_ const char* SQLITE_STDCALL sqlite3_vsnprintf(_In_range_( >=, 1 ) int n,_Pre_writable_size_( n ) char*, _Printf_format_string_ _In_z_ const char*, va_list);
 
 /*
 ** CAPI3REF: Memory Allocation Subsystem
