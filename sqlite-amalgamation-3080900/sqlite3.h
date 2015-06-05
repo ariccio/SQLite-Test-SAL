82cfc4d3bb0b01aff7ff050466f76390cf1bc52a
Alexander Riccio <alexander@riccio.com>
2015-06-05 17:20:00 -0400

    fix sqlite3_context_db_handle

--- sqlite-amalgamation-3080900/sqlite3.h
+++ sqlite-amalgamation-3080900/sqlite3.h
@@ -4387,7 +4387,7 @@ SQLITE_API void *SQLITE_STDCALL sqlite3_user_data(sqlite3_context*);
 ** and [sqlite3_create_function16()] routines that originally
 ** registered the application defined function.
 */
-SQLITE_API sqlite3 *SQLITE_STDCALL sqlite3_context_db_handle(_In_ _Pre_satisfies_( ( p != 0 ) && ( p->pFunc != 0 ) && ( p->pOut != 0 ) ) sqlite3_context*);
+SQLITE_API sqlite3 *SQLITE_STDCALL sqlite3_context_db_handle(_In_ _Pre_satisfies_( ( p != 0 ) && ( p->pFunc != 0 ) && ( p->pOut != 0 ) ) sqlite3_context* p);
 
 /*
 ** CAPI3REF: Function Auxiliary Data
